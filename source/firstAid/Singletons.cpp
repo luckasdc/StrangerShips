95cd5f2a7cd370dc9d8c0db383455e5ea8e3aa7d
luckasdc <mail@luckasdc.be>
2018-08-20 22:48:14 +0200

    manual code cleanup

--- source/firstAid/Singletons.cpp
+++ source/firstAid/Singletons.cpp
@@ -1,11 +1,8 @@
 
 #include "Singletons.h"
 
-
 void Stopwatch::start() {
-
     this->startTime = clock.now().time_since_epoch();
-
 }
 
 long long int Stopwatch::elapsedMilliSeconds() const {
@@ -15,43 +12,23 @@ long long int Stopwatch::elapsedMilliSeconds() const {
     auto difference = std::chrono::duration_cast<std::chrono::milliseconds>(now - then);
     return difference.count();
 }
-
-
 /*
  * Transformation
  */
-
 float Transformation::transformWidth(float original, float screen) {
-
     // check if original doesn't exceed borders
-    //if (original < -4 or original > 4) std::cerr<< "Width exceeds exceeds limits!" << std::endl;
-
-    // map to range of canvas size
     float OldRange = (4 - (-4));
     float NewRange = (screen - 0);
     float newvalue = (((original - (-4)) * NewRange) / OldRange) + 0;
 
-    // assert if newvalue doenst exceed borders
-    //if (newvalue < 0 or newvalue > screen) std::cerr<< "ASSERTION FAILED. New width exceeds limits!" << std::endl;
-
     return newvalue;
-
-
 }
 
 float Transformation::transformHeight(float original, float screen){
-
     // check if original doesn't exceed borders
-    //if (original < -3 or original > 3) std::cerr<< "Width exceeds exceeds limits!" << std::endl;
-
-    // map to range of canvas size
     float OldRange = (3 - (-3));
     float NewRange = (screen - 0);
     float newvalue = (((original - (-3)) * NewRange) / OldRange) + 0;
 
-    // assert if newvalue doenst exceed borders
-    //if (newvalue < 0 or newvalue > screen) std::cerr<< "ASSERTION FAILED. New width exceeds limits!" << std::endl;
-
     return newvalue;
-
 }
